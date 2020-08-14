2606138e5bd1dea6cc1a9042b785585ec90d6d8c
kanjamad bosel <kanjamadpor@gmail.com>
2020-08-14 10:08:55 -0700

    Updates

---
+++ precache-manifest.26af405ffca5da798e01fd0ff1877399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5466150b85b10fb9bc49176fd1e4f20",
+    "url": "/ToDo_ReactJS/index.html"
+  },
+  {
+    "revision": "83d59e07fc3e6295edd1",
+    "url": "/ToDo_ReactJS/static/css/main.9db6e495.chunk.css"
+  },
+  {
+    "revision": "23df4b6786baba996952",
+    "url": "/ToDo_ReactJS/static/js/2.bf101582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo_ReactJS/static/js/2.bf101582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d59e07fc3e6295edd1",
+    "url": "/ToDo_ReactJS/static/js/main.b799d670.chunk.js"
+  },
+  {
+    "revision": "c6c15c26d9482d9c1227",
+    "url": "/ToDo_ReactJS/static/js/runtime-main.872d15ff.js"
+  }
+]);
